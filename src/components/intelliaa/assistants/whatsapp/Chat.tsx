4a4bc49eff6799b0cde5766c233b9e67f504d4a1
Raúl Camacho <raulj.camacho@gmail.com>
2024-12-15 20:36:44 -0500

    Fixed: height Ws config component add margin chat1

--- src/components/intelliaa/assistants/whatsapp/Chat.tsx
+++ src/components/intelliaa/assistants/whatsapp/Chat.tsx
@@ -179,7 +179,7 @@ export default function ChatWsComponent({
   }, [messages]);
 
   return (
-    <Card className='flex flex-col text-muted-foreground dark:bg-[#242322]/80 dark:border-gray-700 dark:shadow-[inset_0_0_20px_rgba(20,184,166,0.2)] overflow-hidden justify-between w-[40%] p-4'>
+    <Card className='flex flex-col text-muted-foreground dark:bg-[#242322]/80 dark:border-gray-700 dark:shadow-[inset_0_0_20px_rgba(20,184,166,0.2)] overflow-hidden justify-between w-[40%] '>
       <CardContent>
         <div className='flex items-center justify-between my-5'>
           <Label htmlFor='assistant-name' className='text-lg'>
