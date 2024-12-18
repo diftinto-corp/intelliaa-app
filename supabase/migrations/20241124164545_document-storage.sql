ce7369f3e0d648d3d5dabb64299d15a732a1c1ea
Raúl Camacho <raulj.camacho@gmail.com>
2024-12-18 15:59:43 -0500

    Fixed: Migrations4

--- supabase/migrations/20241124164545_document-storage.sql
+++ supabase/migrations/20241124164545_document-storage.sql
@@ -10,6 +10,25 @@ drop policy "All logged in users can select" on "public"."pdf_docs";
 
 alter table "public"."pdf_docs" drop constraint "pdf_docs_created_by_fkey";
 
+alter table "public"."pdf_docs" drop constraint "pdf_docs_updated_by_fkey";
+
+alter table "public"."pdf_docs" drop constraint "public_pdf_docs_account_id_fkey";
+
+alter table "public"."pdf_docs" drop constraint "public_pdf_docs_document_storage_id_fkey";
+
+-- Verificación para la restricción s3_key
+DO $$ BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM information_schema.table_constraints 
+        WHERE constraint_name = 'pdf_docs_s3_key_key' 
+        AND table_name = 'pdf_docs'
+    ) THEN
+        alter table "public"."pdf_docs" drop constraint "pdf_docs_s3_key_key";
+    END IF;
+END $$;
+
+-- Verificación para la columna s3_key
 DO $$ BEGIN
     IF EXISTS (
         SELECT 1
@@ -21,21 +40,11 @@ DO $$ BEGIN
     END IF;
 END $$;
 
-alter table "public"."pdf_docs" drop constraint "pdf_docs_updated_by_fkey";
-
-alter table "public"."pdf_docs" drop constraint "public_pdf_docs_account_id_fkey";
-
-alter table "public"."pdf_docs" drop constraint "public_pdf_docs_document_storage_id_fkey";
-
-drop index if exists "public"."pdf_docs_s3_key_key";
-
 alter table "public"."pdf_docs" drop column "account_id";
 
 alter table "public"."pdf_docs" drop column "created_by";
 
 alter table "public"."pdf_docs" drop column "document_storage_id";
-
-alter table "public"."pdf_docs" drop column "s3_key";
 
 alter table "public"."pdf_docs" drop column "updated_by";
 
@@ -50,30 +59,6 @@ alter table "public"."pdf_docs" alter column "id" set default gen_random_uuid();
 alter table "public"."pdf_docs" alter column "updated_at" set default now();
 
 alter table "public"."pdf_docs" add constraint "public_pdf_docs_document_storages_id_fkey" FOREIGN KEY (document_storages_id) REFERENCES document_storages(id) not valid;
-
--- Verificación para la restricción
-DO $$ BEGIN
-    IF EXISTS (
-        SELECT 1 
-        FROM information_schema.table_constraints 
-        WHERE constraint_name = 'pdf_docs_s3_key_key' 
-        AND table_name = 'pdf_docs'
-    ) THEN
-        alter table "public"."pdf_docs" drop constraint "pdf_docs_s3_key_key";
-    END IF;
-END $$;
-
--- Verificación para la columna
-DO $$ BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'pdf_docs'
-        AND column_name = 's3_key'
-    ) THEN
-        alter table "public"."pdf_docs" drop column "s3_key";
-    END IF;
-END $$;
 
 -- Primero, asegurarse de que todos los registros tengan un document_storages_id válido
 INSERT INTO document_storages (id)
